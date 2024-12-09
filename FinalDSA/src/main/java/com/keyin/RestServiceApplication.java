424c443fc8a4d0e01d648f75d4c8bdca63e38d5e
Cap2711 <chelsey.penton@keyin.com>
2024-12-09 12:20:42 -0330

    fixing for working html pages

--- FinalDSA/src/main/java/com/keyin/RestServiceApplication.java
+++ FinalDSA/src/main/java/com/keyin/RestServiceApplication.java
@@ -3,7 +3,7 @@ package com.keyin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.keyin")
 public class RestServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
--- FinalDSA/src/main/java/com/keyin/controller/BTController.java
+++ FinalDSA/src/main/java/com/keyin/controller/BTController.java
@@ -3,35 +3,35 @@ package com.keyin.controller;
 import com.keyin.entities.BinaryNode;
 import com.keyin.entities.PreviousTreeDTO;
 import com.keyin.entities.TreeDTO;
-import com.keyin.logger.CustomLogger;
 import com.keyin.service.BTService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/trees")
 public class BTController {
 
     private final BTService btService;
-    private final CustomLogger customLogger = new CustomLogger();
 
     @Autowired
     public BTController(BTService btService) {
         this.btService = btService;
     }
 
+
     @GetMapping("/enter-numbers")
     public String getEnterNumbersPage() {
-        customLogger.logAction("GET /trees/enter-numbers");
-        return "enter"; // Returns the enter.html template
+        return "enter-numbers";
     }
 
+
     @PostMapping("/process")
-    public ResponseEntity<TreeDTO> processNumbers(@RequestBody List<Integer> numbers) {
+    public ResponseEntity<TreeDTO> processNumbers(@RequestParam List<Integer> numbers) {
         try {
             BinaryNode root = btService.constructBinarySearchTree(numbers);
             btService.saveUserInputAndTree(numbers, root);
@@ -40,26 +40,29 @@ public class BTController {
             responseDTO.setInputValues(numbers);
             responseDTO.setRoot(root);
 
-            customLogger.logAction("POST /trees/process");
             return ResponseEntity.ok(responseDTO);
         } catch (Exception e) {
-            customLogger.logError("Error in POST /trees/process");
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().build();
         }
     }
+
 
     @GetMapping("/previous")
     public ResponseEntity<List<PreviousTreeDTO>> getAllPreviousTrees() {
         try {
             List<PreviousTreeDTO> previousTrees = btService.getAllPreviousTrees();
-
-            customLogger.logAction("GET /trees/previous");
             return ResponseEntity.ok(previousTrees);
         } catch (Exception e) {
-            customLogger.logError("Error in GET /trees/previous");
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().build();
         }
+    }
+
+
+    @RequestMapping("/error")
+    public String handleError(Model model) {
+        model.addAttribute("errorMessage", "An unexpected error occurred.");
+        return "error";  // This will render 'error.html'
     }
 }
